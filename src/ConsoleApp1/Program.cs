4b85bfe9500f11df52a3a8fcfcf9c9fd13116e2d
Yavor Fingarov <yavor.fingarov@outlook.com>
2025-01-07 22:27:08 +0200

    Initial implementation

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
